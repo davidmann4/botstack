cadbc899781139402503eedbb5c9cd6e470e3b9b
davidmann4 <daaavid.mann@gmail.com>
2016-08-01 15:14:45 +0200

    new gem for fb-messenger :whale2:

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  mount Facebook::Messenger::Server, at: 'bot'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
