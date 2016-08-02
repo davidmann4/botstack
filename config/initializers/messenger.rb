135facb903c05f65c88a199dc634722d9902dce4
davidmann4 <daaavid.mann@gmail.com>
2016-08-02 11:51:04 +0200

    Added Console msg which shows the verify token. :rocket:

--- config/initializers/messenger.rb
+++ config/initializers/messenger.rb
@@ -1,4 +1,5 @@
 Messenger.configure do |config|
-  config.verify_token      = '' #will be used in webhook verifiction
-  config.page_access_token = ''
+  config.verify_token      = Settings.verify_token
+  config.page_access_token = Settings.page_access_token
+  puts "\e[36m" + "Verify the bot with the following verify_token :" + Settings.page_access_token + "\e[0m"
 end
