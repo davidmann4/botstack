2317eb186c0f7f99f15ad7f904923f49e5cc5541
davidmann4 <daaavid.mann@gmail.com>
2016-08-29 15:48:09 +0200

    basic starterkit

--- lib/bot/bot_logic.rb
+++ lib/bot/bot_logic.rb
@@ -3,18 +3,7 @@
 class BotLogic < BaseBotLogic
 
 	def self.bot_logic
-		#reply_message "Hello World."
-		search_request_on_website(
-		    url: "http://www.chefkoch.de/",
-		    form_name: 'searchform',
-		    result_css_selector: '.search-list-item > a',
-		    image_css_selector: 'img'
-		)
-
-		handle_search_result(
-		    url: "http://www.chefkoch.de",
-		    result_css_selector: ".ingredients__container"
-		)	
+		reply_message "Hello World."
 	end
 
 end
