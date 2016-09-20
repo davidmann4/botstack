79f16a342f4df7213d061841912a7dd786978441
davidmann4 <daaavid.mann@gmail.com>
2016-09-20 10:02:53 +0200

    unknown changes

--- lib/bot/bot_logic.rb
+++ lib/bot/bot_logic.rb
@@ -2,8 +2,22 @@
 
 class BotLogic < BaseBotLogic
 
-	def self.bot_logic
-		reply_message "Hello World."
-	end
+  def self.bot_logic
+    #reply_message "Hello World."
+    #binding.pry
+
+    search_request_on_website(
+        url: "http://www.chefkoch.de/",
+        form_name: 'searchform',
+        result_css_selector: '.search-list-item > a',
+        image_css_selector: 'img',
+        button_text: 'Rezept anzeigen'
+    )
+
+    handle_search_result(
+        url: "http://www.chefkoch.de",
+        result_css_selector: ".ingredients__container"
+    )
+  end
 
 end
